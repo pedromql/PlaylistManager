025ec87491971fbe86052e07e5367c354c11de52
pedromql <pedromql@gmail.com>
2017-04-12 02:39:13 +0100

    edit playlist

--- api/playlist.py
+++ api/playlist.py
@@ -59,3 +59,87 @@ def create_playlist():
 		response.status_code = 500
 
 		return response
+
+
+
+@app.route("/api/playlist", methods=['PUT'])
+def edit_playlist():
+	try:
+
+		id = request.json['id']
+		token = request.json['token']
+		name = request.json['name']
+
+		session = create_session()
+
+		#check if username exists
+		user = session.query(User).filter(User.token == token).first()
+		playlist = session.query(Playlist).filter(Playlist.id == id).first()
+		#if token corresponds create playlist
+		if user is not None and playlist is not None:
+			if playlist.user_id == user.id:
+				playlist.name = name
+				session.commit()
+				session.close()
+
+				response_data = {
+					'result' : 'Success',
+					'message' : 'Playlist '+name+' changed successfuly'
+				}
+
+				response = jsonify(response_data)
+				response.status_code = 200
+
+				return response
+
+			else:
+				session.close()
+
+				response_data = {
+					'result' : 'Error',
+					'message' : 'Playlist does not belong to the user'
+				}
+
+				response = jsonify(response_data)
+				response.status_code = 403
+
+				return response
+				
+		#if user exists return error            
+		elif user is None:
+			session.close()
+
+			response_data = {
+				'result' : 'Error',
+				'message' : 'Invalid Token'
+			}
+
+			response = jsonify(response_data)
+			response.status_code = 401
+
+			return response
+		else:
+			session.close()
+
+			response_data = {
+				'result' : 'Error',
+				'message' : 'Playlist does not exist'
+			}
+
+			response = jsonify(response_data)
+			response.status_code = 403
+
+			return response
+
+
+	except Exception as e:
+		print(e)
+		response_data = {
+				'result' : 'Error',
+				'message' : 'Server Error'
+		}
+
+		response = jsonify(response_data)
+		response.status_code = 500
+
+		return response
