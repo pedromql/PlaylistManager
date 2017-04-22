b29a83541dc216d68b368f68bc6194c6962f96b5
filipes <f_sequeira994@hotmail.com>
2017-04-22 19:50:38 +0100

    server config

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 from api import application
 import logging
+import os
 
 if __name__ == "__main__":
-
+    os.makedirs('uploads/Music', exist_ok=True)
     application.debug = False
 
     application.run(host="0.0.0.0", port=8080, processes=4)
